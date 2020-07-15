6bf99f084c4e58c07b7fb42d67d40aeaabc4d716
denislgx <denislg_@hotmail.com>
2020-07-15 19:16:06 -0300

    navbar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
-    const [people, setPeople] = useState('');
 
-    useEffect(() => {
-        fetch('/api/people')
-            .then(res => res.json())
-            .then(people => setPeople(people));
-    }, []);
+    // CONNECTION TEST WITH BACKEND
+    // const [people, setPeople] = useState('');
 
-    console.log(people)
+    // useEffect(() => {
+    //     fetch('/api/people')
+    //         .then(res => res.json())
+    //         .then(people => setPeople(people));
+    // }, []);
+
+    // console.log(people)
+
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>PEOPLE</h2>
-                <ul>
-                    {people && people.map(person => <h1>{person.name}</h1>)}
-                </ul>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
+        <div>
+            <Navbar />
         </div>
     );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <div className='wrapper'>
+            <div className="logo_wrapper">
+                <div className="logo">TRIVIA-GAME</div>
+            </div>
+            <div className="registration_wrapper">
+                <div className="sign_up">SIGN-UP</div>
+                <div className="register">REGISTER</div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
